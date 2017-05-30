ab026b5df471f64677a24033da19b79985dbe4fb
Quentin Peter <qaep2@cam.ac.uk>
2017-05-30 14:22:07 +0100

    added lib as tmp

--- source/launch.py
+++ source/launch.py
@@ -18,6 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import sys
+sys.path.append('../lib/')
 from PyQt5 import QtWidgets
 from application_interface import ApplicationWindow
 
