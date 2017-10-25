1ecc1e8b39bd634683c3f623341510a5866194bc
Quentin Peter <qaep2@cam.ac.uk>
2017-10-25 13:57:48 +0100

    added red cross

--- source/application_interface.py
+++ source/application_interface.py
@@ -19,82 +19,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from PyQt5 import QtCore, QtWidgets, QtGui
 import numpy as np
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
 from application_delegate import application_delegate
 from myWidgets import LightWidget, doubleSelector
 from functools import partial
-#%%
-        
 
-#==============================================================================
-# Plot canevas
-#==============================================================================
-
-class MyMplCanvas(FigureCanvas):
-    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self._axes = fig.add_subplot(111)
-        
-        self.compute_initial_figure()
-
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-
-        FigureCanvas.setSizePolicy(self,
-                                   QtWidgets.QSizePolicy.Expanding,
-                                   QtWidgets.QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
-
-    def compute_initial_figure(self):
-        pass
-    
-class imageCanvas(MyMplCanvas):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.clear()
-        self._lastim=np.zeros((2,2))
-        self._autoc = False
-        
-    def imshow(self, im , vmax = None):
-        self._lastim=im
-        self.figure.clear()
-        self._axes = self.figure.add_subplot(111)
-        self._imhandle=self._axes.imshow(im, vmax = vmax)
-        self._axes.axis('image')
-        self.figure.colorbar(self._imhandle)
-        self.draw()
-       
-#    @profile
-    def frameshow(self, im):
-        self._lastim=im
-        if self._imhandle is not None:
-            self._imhandle.set_data(im[::2, ::2])
-#            if self._autoc:
-#                self._imhandle.set_clim(im.min(), im.max())
-            self._axes.draw_artist(self._imhandle)
-            self.blit(self._axes.bbox)
-        else:
-            self.imshow(im[::2, ::2], vmax = 255)
-            
-    def clear(self):
-        self._imhandle=None
-        self.figure.clear()
-        self._axes = self.figure.add_subplot(111)
-        self.draw()
-        
-    def plot(self, X, Y, fmt='-', axis='normal', **kwargs):
-        if self._imhandle is not None:
-            self.clear()
-        self._axes.plot(X, Y, fmt, **kwargs)
-        self._axes.axis(axis)
-        self.draw()
-        
-    def get_im(self):
-        return self._lastim
-        
+from ImageCanvas import ImageCanvas
         
 #==============================================================================
 # Tabs            
@@ -841,7 +770,7 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 #        self.statusBar().showMessage("Hello", 2000)
 
         #Create canevas
-        imCanvas=imageCanvas()
+        imCanvas=ImageCanvas()
         
         #create appication delegate
         self.application_delegate=application_delegate(imCanvas)
